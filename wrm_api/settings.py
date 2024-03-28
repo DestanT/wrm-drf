a4d53c9722097e47734098492af697b14f4ecbda
Destan <destantezel@googlemail.com>
2024-03-28 13:01:40 +0000

    FFix secret key variable to reflect name in env.py

--- wrm_api/settings.py
+++ wrm_api/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 if os.path.exists('env.py'):
-    import env # pylint: disable=unused-import
+    import env
 
 # Cloudinary settings
 CLOUDINARY_STORAGE = {
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
