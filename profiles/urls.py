f9f69f9a7699f9683d290b5bffef66a746407522
Destan <destantezel@googlemail.com>
2024-03-28 14:29:31 +0000

    Add path to 'profiles/<int:pk>' for detailed view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from profiles.views import ProfileList
+from profiles.views import ProfileList, ProfileDetail
 
 urlpatterns = [
     path('profiles/', ProfileList.as_view()),
+    path('profiles/<int:pk>/', ProfileDetail.as_view()),
 ]
