04fe09591dbc83d889954050a6d7f821280e2b54
Destan <destantezel@googlemail.com>
2024-03-28 13:10:05 +0000

    Create Pprofile model and signal creation of profile object for each new user

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='wrm/profile', default='../default_profile_lcovgw'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+# Create a profile for each new user
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
