e8dd54a208c0b5630fcdb09b3a5a0cf3c6cce500
Destan <destantezel@googlemail.com>
2024-04-01 21:29:47 +0100

    Create rating model

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from playlists.models import Playlist
 
-# Create your models here.
+User = get_user_model()
+
+class Rating(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='ratings')
+    score = models.PositiveIntegerField(
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('owner', 'playlist')
+
+    def __str__(self):
+        return f"{self.owner} rated {self.playlist} a {self.score}"
