0fe8645bcd77549180ea69583c79720df514385e
Destan <destantezel@googlemail.com>
2024-04-01 21:47:12 +0100

    Create season model

--- seasons/models.py
+++ seasons/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Season(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    participants = models.ManyToManyField(User, through='SeasonParticipant', through_fields=('season', 'user'))
+    created_on = models.DateTimeField(auto_now_add=True)
+    ended_on = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.name}"
