814db2a11b6c35f5c59e54dcf9f5772fc81f8b3a
Destan <destantezel@googlemail.com>
2024-04-01 21:06:11 +0100

    Create playlist model

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+User = get_user_model()
+
+class Playlist(models.Model):
+    PLAYLIST_TYPE = (
+        ('p', 'Playlist'),
+        ('a', 'Album'),
+    )
+
+    spotify_id = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    image = models.URLField(max_length=255)
+    genres = ArrayField(models.CharField(max_length=255), blank=True, default=list)
+    type = models.CharField(max_length=1, choices=PLAYLIST_TYPE)
+    added_on = models.DateTimeField(auto_now_add=True)
+    total_tracks = models.IntegerField()
+    duration = models.BigIntegerField()
+    external_url = models.URLField(max_length=255)
+
+    class Meta:
+        ordering = ['-added_on']
+
+    def __str__(self):
+        return f"{self.name}"
