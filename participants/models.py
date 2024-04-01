efa9cf32bcace23587aff0e134bbb0938949b365
Destan <destantezel@googlemail.com>
2024-04-01 21:46:12 +0100

    Create season participants model

--- participants/models.py
+++ participants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class SeasonParticipant(models.Model):
+    season = models.ForeignKey('Season', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    joined_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'season')
