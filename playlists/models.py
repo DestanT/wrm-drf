31cc0479ad90ea00dc844b2aadd14b58f2d9f459
Destan <destantezel@googlemail.com>
2024-04-02 13:46:07 +0100

    Remove ArrayField - SQLite incompatible - will re-implement in production and postgreSQL

--- playlists/models.py
+++ playlists/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.contrib.postgres.fields import ArrayField
 
 User = get_user_model()
 
@@ -14,7 +13,6 @@ class Playlist(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     image = models.URLField(max_length=255)
-    genres = ArrayField(models.CharField(max_length=255), blank=True, default=list)
     type = models.CharField(max_length=1, choices=PLAYLIST_TYPE)
     added_on = models.DateTimeField(auto_now_add=True)
     total_tracks = models.IntegerField()
