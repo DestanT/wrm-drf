2145d6f7173a36742e25c68d75188a8d3b69d4a2
Destan <destantezel@googlemail.com>
2024-04-02 15:15:40 +0100

    Update unique_together to UniqueContraint and define custom save method

--- participants/models.py
+++ participants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from seasons.models import Season
 
@@ -10,4 +11,16 @@ class SeasonParticipant(models.Model):
     joined_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('user', 'season')
+        constraints = [
+            models.UniqueConstraint(fields=['season', 'user'], name='unique_user_in_season')
+        ]
+
+    def save(self, *args, **kwargs):
+        if self.user in self.season.participants.all():
+            raise ValidationError('User is already participating in this season')
+        if self.season.ended_on is not None:
+            raise ValidationError('Season has already ended, cannot add participants')
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.user} participated in "{self.season}"'
