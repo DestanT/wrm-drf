b161c5eb777108478c696d77a3893d71cc508884
Destan <destantezel@googlemail.com>
2024-04-03 00:42:20 +0100

    Create playlist list and detail views

--- playlists/views.py
+++ playlists/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from wrm_api.permissions import IsOwnerOrReadOnly
+from .models import Playlist
+from .serializers import PlaylistSerializer
 
-# Create your views here.
+
+class PlaylistList(generics.ListCreateAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PlaylistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
+    permission_classes = [IsOwnerOrReadOnly]
