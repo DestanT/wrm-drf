6bc513c23828979926d2e94d557fb18dd513177d
Destan <destantezel@googlemail.com>
2024-04-03 01:02:31 +0100

    Create season list and details views

--- seasons/views.py
+++ seasons/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Season
+from .serializers import SeasonSerializer
 
-# Create your views here.
+
+class SeasonList(generics.ListAPIView):
+    queryset = Season.objects.all()
+    serializer_class = SeasonSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class SeasonDetail(generics.RetrieveUpdateAPIView):
+    queryset = Season.objects.all()
+    serializer_class = SeasonSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
