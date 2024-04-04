9b1e1407f3547e6d656194c61d4582f359484826
Destan <destantezel@googlemail.com>
2024-04-04 19:04:12 +0100

    Move SeasonParticipantSerializer to 'seasons' app

--- seasons/serializers.py
+++ seasons/serializers.py
@@ -1,5 +1,7 @@
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
-from .models import Season
+from .models import Season, SeasonParticipant
 
 
 class SeasonSerializer(serializers.ModelSerializer):
@@ -9,3 +11,23 @@ class SeasonSerializer(serializers.ModelSerializer):
             'id', 'name', 'description', 'participants',
             'created_on', 'ended_on',
         ]
+
+class SeasonParticipantSerializer(serializers.ModelSerializer):
+    season = serializers.ReadOnlyField(source='season.name')
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = SeasonParticipant
+        fields = ['id', 'season', 'user', 'joined_on']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError as exc:
+            raise serializers.ValidationError({
+                'detail': 'User is already participating in this season'
+            }) from exc
+        except ValidationError as exc:
+            raise serializers.ValidationError({
+                'detail': 'Season has already ended, cannot add participants'
+            }) from exc
