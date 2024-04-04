3b9b747a60089f2263f8db2ec59e63936a6cf518
Destan <destantezel@googlemail.com>
2024-04-04 22:53:47 +0100

    Add search_fields, list_filters etc to the admin panel

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Playlist
 
-admin.site.register(Playlist)
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'type', 'added_on')
+    list_filter = ('owner', 'type', 'added_on')
+    search_fields = ('name', 'owner__username')
+    date_hierarchy = 'added_on'
+    ordering = ('-added_on',)
