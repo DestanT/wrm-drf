15be0c7e854dd486ff3039bcad6481e2c713445f
Destan <destantezel@googlemail.com>
2024-04-04 19:10:35 +0100

    Add paths to include the urls from seasons and playlists

--- wrm_api/urls.py
+++ wrm_api/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('profiles.urls')),
+    path('', include('seasons.urls')),
+    path('', include('playlists.urls')),
 ]
