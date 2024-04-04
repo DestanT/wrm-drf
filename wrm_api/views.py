4e0cbbc3c2a8297600fe4b34e199d49ed84143cc
Destan <destantezel@googlemail.com>
2024-04-04 19:22:36 +0100

    Include the endpoints of the api in the homescreen

--- wrm_api/views.py
+++ wrm_api/views.py
@@ -5,5 +5,14 @@ from rest_framework.response import Response
 @api_view()
 def root_route(request):
     return Response({
-        'message': 'Welcome to the WRM API.'
+        'message': 'Welcome to the WRM API.',
+        'endpoints': {
+            'profiles/': 'List of profiles',
+            'profiles/<int:pk>/': 'Detail of a profile',
+            'seasons/': 'List of seasons',
+            'seasons/<int:pk>/': 'Detail of a season',
+            'seasons/<int:pk>/participants/': 'List of participants in the season',
+            'playlists/': 'List of playlists',
+            'playlists/<int:pk>/': 'Detail of a playlist',
+        }
     })
