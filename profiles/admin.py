721f191c0055a4df235ac7eaad83783c8ea88350
Destan <destantezel@googlemail.com>
2024-04-04 23:08:17 +0100

    Add search_fields and list_display etc to admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'created_at')
+    search_fields = ('owner__username',)
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
