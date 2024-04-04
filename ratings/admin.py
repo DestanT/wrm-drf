a395822e3217a045bbfc501f4e6dce1c5dfa48b9
Destan <destantezel@googlemail.com>
2024-04-04 23:58:14 +0100

    Add list_display, filter, and search_fields etc to the rating admin panel

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Rating
 
-admin.site.register(Rating)
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'score', 'playlist', 'created_at')
+    list_filter = ('owner', 'playlist__season__name')
+    search_fields = ('owner__username', 'playlist__name', 'playlist__season__name')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
