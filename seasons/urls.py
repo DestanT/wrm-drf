ad82aa6684c90c17a0b5002221cdd46186fd4869
Destan <destantezel@googlemail.com>
2024-04-04 19:04:59 +0100

    Add the path to participants to a specific season

--- seasons/urls.py
+++ seasons/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SeasonList, SeasonDetail
+from .views import SeasonList, SeasonDetail, SeasonParticipantList
 
 urlpatterns = [
     path('seasons/', SeasonList.as_view()),
     path('seasons/<int:pk>', SeasonDetail.as_view()),
+    path('seasons/<int:pk>/participants/', SeasonParticipantList.as_view()),
 ]
