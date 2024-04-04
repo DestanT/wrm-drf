ad4d2dab73f328acb77f0d3e4ff074d1f8ec686d
Destan <destantezel@googlemail.com>
2024-04-04 23:36:31 +0100

    Define save method and allow users to update existing obj, while restricting them to one rating per playlist

--- ratings/models.py
+++ ratings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from playlists.models import Playlist
@@ -22,5 +23,11 @@ class Rating(models.Model):
             models.UniqueConstraint(fields=['owner', 'playlist'], name='unique_rating_per_user_per_playlist')
         ]
 
+    # Allows updates, while restricting the user to one rating per playlist
+    def save(self, *args, **kwargs):
+        if Rating.objects.filter(owner=self.owner, playlist=self.playlist).exclude(pk=self.pk).exists():
+            raise ValidationError('You can only rate a playlist once')
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.owner} rated {self.playlist} a {self.score}"
+        return f'{self.owner} rated {self.playlist} a {self.score}'
