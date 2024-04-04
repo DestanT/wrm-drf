11d41b08429055136b63da363212daec7dac245e
Destan <destantezel@googlemail.com>
2024-04-05 00:33:13 +0100

    Update permission_classes to IsAdminOrReadOnly

--- seasons/views.py
+++ seasons/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from wrm_api.permissions import IsAdminOrReadOnly
 from .models import Season, SeasonParticipant
 from .serializers import SeasonSerializer, SeasonParticipantSerializer
 
@@ -6,18 +7,18 @@ from .serializers import SeasonSerializer, SeasonParticipantSerializer
 class SeasonList(generics.ListAPIView):
     queryset = Season.objects.all()
     serializer_class = SeasonSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class SeasonDetail(generics.RetrieveUpdateAPIView):
     queryset = Season.objects.all()
     serializer_class = SeasonSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class SeasonParticipantList(generics.ListAPIView):
     serializer_class = SeasonParticipantSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_queryset(self):
         season_id = self.kwargs.get('pk')
