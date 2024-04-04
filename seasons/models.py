2cdd57dc89c1789345ab6097b400bfbf2dd8bc9c
Destan <destantezel@googlemail.com>
2024-04-04 19:03:21 +0100

    Move SeasonParticipant model to 'seasons' app, adjust through field in Season m2m field

--- seasons/models.py
+++ seasons/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -8,7 +9,7 @@ class Season(models.Model):
     description = models.TextField(blank=True)
     participants = models.ManyToManyField(
         User,
-        through='participants.SeasonParticipant',
+        through='SeasonParticipant',
         through_fields=('season', 'user')
     )
     created_on = models.DateTimeField(auto_now_add=True)
@@ -19,3 +20,24 @@ class Season(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class SeasonParticipant(models.Model):
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    joined_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['season', 'user'], name='unique_user_in_season')
+        ]
+
+    def save(self, *args, **kwargs):
+        if self.user in self.season.participants.all():
+            raise ValidationError('User is already participating in this season')
+        if self.season.ended_on is not None:
+            raise ValidationError('Season has already ended, cannot add participants')
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.user} participated in "{self.season}"'
