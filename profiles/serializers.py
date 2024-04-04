112369891ccfd7e5831fc5db96c56795af44edfb
Destan <destantezel@googlemail.com>
2024-04-04 23:07:42 +0100

    Add image validation to  profile serializer, nothing larger than 4096 x 4096px and 2mb

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,6 +10,15 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError('Image cannot be larger than 2MB')
+        if value.image.width > 4096:
+            raise serializers.ValidationError('Image width cannot be larger than 4096px')
+        if value.image.height > 4096:
+            raise serializers.ValidationError('Image height cannot be larger than 4096px')
+        return value
+
     class Meta:
         model = Profile
         fields = ['id', 'owner', 'is_owner', 'created_at', 'image']
