aef8a2d788ec67eccbf70ae06ed11b990e163a1b
Destan <destantezel@googlemail.com>
2024-04-05 01:22:40 +0100

    Add list_display, filter, and search_fields etcs to the admin panel

--- seasons/admin.py
+++ seasons/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Season, SeasonParticipant
 
-admin.site.register(Season)
-admin.site.register(SeasonParticipant)
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_on', 'ended_on')
+    list_filter = ('name', 'participants__username')
+    search_fields = ('name', 'description')
+    date_hierarchy = 'created_on'
+    ordering = ('-created_on',)
+
+@admin.register(SeasonParticipant)
+class SeasonParticipantAdmin(admin.ModelAdmin):
+    list_display = ('season', 'user', 'joined_on')
+    list_filter = ('season', 'user')
+    search_fields = ('season__name', 'user__username')
+    date_hierarchy = 'joined_on'
+    ordering = ('-joined_on',)
