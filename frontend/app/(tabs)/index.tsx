c416db751773feccb15917a2fded31d5a3995037
Destan <destantezel@googlemail.com>
2024-05-16 22:23:17 +0100

    Create SpotifyAuthButton to handle Spotify Auth flow

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,31 +1,71 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { Button } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import { useEffect, useState } from 'react';
+import { axiosReq } from '@/api/axiosDefaults';
 
-export default function TabOneScreen() {
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
+
+const CLIENT_ID = 'bff492e74a8345b1af5b7084adcc5cb5';
+
+export default function SpotifyAuthButton() {
+  const [accessToken, setAccessToken] = useState(null);
+
+  // Reference: https://docs.expo.dev/guides/authentication/#spotify
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: CLIENT_ID,
+      scopes: ['user-read-private', 'user-read-email', 'streaming'],
+      usePKCE: true,
+      redirectUri: makeRedirectUri({
+        scheme: 'we-rate-music',
+      }),
+    },
+    discovery
+  );
+
+  // Get Spotify access token after successful authentication
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      const code_verifier = request?.codeVerifier
+
+      const getAccessToken = async () => {
+        try {
+          const response = await axiosReq.get('auth/spotify/callback/', {
+            params: { code, code_verifier }
+          });
+
+          if (response.status === 200) {
+            setAccessToken(response.data.access_token);
+          } else {
+            console.error('Error fetching access token:', response.data);
+          }
+        } catch (error) {
+          console.error('Error fetching access token:', error);
+        }
+    }
+    getAccessToken();
+  }
+  }, [response]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+        />
+      {response?.type === 'success' ? <Text>Response:{response.params.code}</Text> : null}
+      {accessToken ? <Text>ACCESS_TOKEN:{accessToken}</Text> : null}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
