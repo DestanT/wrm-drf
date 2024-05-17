1f2b9e03b42c045e80a9f020b3baf6d8660479de
Destan <destantezel@googlemail.com>
2024-05-17 02:24:35 +0100

    CConvert (tabs)/index.tsx's SpotifyAuth logic into it's own hook in .ts file

---
+++ frontend/hooks/useSpotifyAuth.ts
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import {
+  AuthRequest,
+  AuthRequestConfig,
+  AuthSessionResult,
+  makeRedirectUri,
+  useAuthRequest,
+} from 'expo-auth-session';
+import { axiosReq } from '@/api/axiosDefaults';
+
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
+
+const CLIENT_ID = 'bff492e74a8345b1af5b7084adcc5cb5';
+
+interface SpotifyAuthResponse {
+  accessToken: string | null;
+  promptAsync: () => Promise<AuthSessionResult>;
+  request: AuthRequest | null;
+  response: AuthSessionResult | null;
+}
+
+// Reference: https://docs.expo.dev/guides/authentication/#spotify
+export const useSpotifyAuth = (): SpotifyAuthResponse => {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  // TS Reference: https://docs.expo.dev/versions/latest/sdk/auth-session/#authrequestconfig
+  const config: AuthRequestConfig = {
+    clientId: CLIENT_ID,
+    scopes: ['user-read-private', 'user-read-email', 'streaming'],
+    usePKCE: true,
+    redirectUri: makeRedirectUri({
+      scheme: 'we-rate-music',
+    }),
+  };
+
+  const [request, response, promptAsync] = useAuthRequest(config, discovery);
+
+  // Get Spotify access token after successful authentication
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      const code_verifier = request?.codeVerifier;
+
+      const getAccessToken = async () => {
+        try {
+          const response = await axiosReq.get('auth/spotify/callback/', {
+            params: { code, code_verifier },
+          });
+
+          if (response.status === 200) {
+            setAccessToken(response.data.access_token);
+          } else {
+            console.error('Error fetching access token:', response.data);
+          }
+        } catch (error) {
+          console.error('Error fetching access token:', error);
+        }
+      };
+      getAccessToken();
+    }
+  }, [response]);
+
+  return {
+    accessToken,
+    promptAsync,
+    request,
+    response,
+  };
+};
