16f0519fa1024c6be44316882e081990b75ec5ba
Destan <destantezel@googlemail.com>
2024-05-18 23:33:50 +0100

    Add useSession hook's sign-out button

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -2,18 +2,27 @@
 import { Button } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { useSpotifyAuth } from '@/hooks/useSpotifyAuth';
+import { useSession } from '@/contexts/AuthContext';
 
 
 export default function TabOneScreen() {
   const { accessToken, promptAsync, request, response } = useSpotifyAuth();
+  const { signOut } = useSession();
 
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Button
         disabled={!request}
-        title="Login"
+        title="Spotify Auth"
         onPress={() => {
           promptAsync();
+        }}
+        />
+      <Button
+        title="Sign Out"
+        onPress={() => {
+          // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
+          signOut();
         }}
         />
       {response?.type === 'success' ? <Text>Response:{response.params.code}</Text> : null}
