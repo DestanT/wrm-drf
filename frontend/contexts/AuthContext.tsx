ca526b6a709896b7eecc72c129b114e7abd0d86f
Destan <destantezel@googlemail.com>
2024-05-19 22:53:45 +0100

    Add signIn logic - POST to dj-rest-auth api endpoint

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,14 +1,15 @@
 
 import { createContext, useContext } from 'react';
 import { useStorageState } from '../hooks/useStorageState';
+import axios from 'axios';
 
 const AuthContext = createContext<{
-  signIn: () => void;
+  signIn: (username: string, password: string) => Promise<void>;
   signOut: () => void;
   session?: string | null;
   isLoading: boolean;
 }>({
-  signIn: () => null,
+  signIn: () => Promise.resolve(),
   signOut: () => null,
   session: null,
   isLoading: false,
@@ -29,13 +30,23 @@ export function useSession() {
 export function SessionProvider(props: React.PropsWithChildren) {
   const [[isLoading, session], setSession] = useStorageState('session');
 
+  const signIn = async (username: string, password: string) => {
+    try {
+      const response = await axios.post("dj-rest-auth/login/", { username, password
+      });
+
+      // Save the session token
+      setSession(response.data.key);
+    } catch (error) {
+      console.error('Failed to sign in:', error);
+      setSession(null);
+    }
+  }
+
   return (
     <AuthContext.Provider
       value={{
-        signIn: () => {
-          // Perform sign-in logic here
-          setSession('xxx');
-        },
+        signIn,
         signOut: () => {
           setSession(null);
         },
