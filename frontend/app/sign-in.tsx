6289c10fe94cca9fa2ca1386182f0c9493474960
Destan <destantezel@googlemail.com>
2024-05-19 22:55:49 +0100

    Add the LoginForm component to the '/sign-in' route

--- frontend/app/sign-in.tsx
+++ frontend/app/sign-in.tsx
@@ -1,21 +1,10 @@
-import { router } from 'expo-router';
-import { Text, View } from 'react-native';
-
-import { useSession } from '@/contexts/AuthContext';
+import { View } from 'react-native';
+import LoginForm from '../components/form/LoginForm';
 
 export default function SignIn() {
-  const { signIn } = useSession();
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text
-        onPress={() => {
-          signIn();
-          // Navigate after signing in. You may want to tweak this to ensure sign-in is
-          // successful before navigating.
-          router.replace('/');
-        }}>
-        Sign In
-      </Text>
+      <LoginForm />
     </View>
   );
 }
