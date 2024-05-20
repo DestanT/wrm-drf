47ac512a53e65365b8fffa7b94530989b08b18e9
Destan <destantezel@googlemail.com>
2024-05-21 00:39:06 +0100

    Add url paths for api/token/ and ../refresh/ for new login and refresh endpoints

--- wrm_api/urls.py
+++ wrm_api/urls.py
@@ -16,14 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import root_route
+
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api/api-auth/', include('rest_framework.urls')),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')), # Currently not in use
     path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('profiles.urls')),
     path('api/', include('seasons.urls')),
     path('api/', include('playlists.urls')),
