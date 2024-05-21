e41171f4c87bdeb862056f1dc091b100f5bfb7ba
Destan <destantezel@googlemail.com>
2024-05-21 18:21:17 +0100

    Add generic button to redirect to 'sign-up/' page

--- frontend/app/sign-in.tsx
+++ frontend/app/sign-in.tsx
@@ -1,10 +1,12 @@
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import LoginForm from '../components/form/LoginForm';
+import { router } from 'expo-router';
 
 export default function SignIn() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <LoginForm />
+      <Button title="Sign Up" onPress={() => router.replace('/sign-up')} />
     </View>
   );
 }
