caa78ecb59fca075948b30d990d5bd54ee50ae33
Destan <destantezel@googlemail.com>
2024-05-21 16:37:35 +0100

    Import getStorageItemAsync and attach to the auth headers as an axios interceptor

--- frontend/api/axiosDefaults.tsx
+++ frontend/api/axiosDefaults.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
+import { getStorageItemAsync } from '@/hooks/useStorageState';
 
 const baseURL = Platform.OS === 'android' ? `${process.env.EXPO_PUBLIC_ANDROID_URL}` : `${process.env.EXPO_PUBLIC_LOCALHOST_URL}`;
 
@@ -7,5 +8,19 @@ axios.defaults.baseURL = baseURL;
 axios.defaults.headers.post['content-type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create();
+const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+// Add interceptor to include JWT token in the headers
+axiosReq.interceptors.request.use(
+    async (config) => {
+        const session = await getStorageItemAsync('session')
+        if (session) {
+            config.headers.Authorization = `Bearer ${session.access}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export { axiosReq };
