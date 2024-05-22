4906bd56036c949c7b1ac03598c7a8384af35146
Destan <destantezel@googlemail.com>
2024-05-23 00:57:42 +0100

    Add search-spotify api endpoint

--- api_auth/urls.py
+++ api_auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import authorise_spotify, get_access_token, refresh_access_token
+from .views import authorise_spotify, get_access_token, refresh_access_token, search_spotify
 
 urlpatterns = [
     path('auth/spotify/', authorise_spotify, name='authorise-spotify'),
     path('auth/spotify/callback/', get_access_token, name='get-spotify-access-token'),
     path('auth/spotify/refresh/', refresh_access_token, name='refresh-spotify-access-token'),
+    path('auth/spotify/search/', search_spotify, name='search-spotify'),
 ]
