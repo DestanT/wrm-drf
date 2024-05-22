514a36cbbc6ce427dc8abf4d8cca7e6b79fabcaa
Destan <destantezel@googlemail.com>
2024-05-23 00:59:35 +0100

    Delete axios.interceptors, and move to AuthContext.tsx

--- frontend/api/axiosDefaults.tsx
+++ frontend/api/axiosDefaults.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
-import { getStorageItemAsync } from '@/hooks/useStorageState';
 
 const baseURL = Platform.OS === 'android' ? `${process.env.EXPO_PUBLIC_ANDROID_URL}` : `${process.env.EXPO_PUBLIC_LOCALHOST_URL}`;
 
@@ -8,19 +7,5 @@ axios.defaults.baseURL = baseURL;
 axios.defaults.headers.post['content-type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
-const axiosReq = axios.create();
-export const axiosRes = axios.create();
-
-// Add interceptor to include JWT token in the headers
-axiosReq.interceptors.request.use(
-    async (config) => {
-        const session = await getStorageItemAsync('session')
-        if (session) {
-            config.headers.Authorization = `Bearer ${session.access}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
-export { axiosReq };
+export const axiosRequest = axios.create();
+export const axiosResponse = axios.create();
