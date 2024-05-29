0815f16dfe4862e430cfedc567cc4072da2a1995
Destan <destantezel@googlemail.com>
2024-05-30 00:58:22 +0100

    Refine refresh token logic in interceptor to only refresh when needed, also delete and simplify response interceptor

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,8 +1,8 @@
-import { createContext, useContext, useEffect, useMemo } from 'react';
-import { getStorageItemAsync, useStorageState } from '../hooks/useStorageState';
+import { createContext, useContext, useEffect } from 'react';
+import { useStorageState } from '../hooks/useStorageState';
 import axios from 'axios';
-import 'core-js/stable/atob'; // Required for atob to work in React Native
-import { axiosRequest } from '@/api/axiosDefaults';
+import { axiosInstance } from '@/api/axiosDefaults';
+import { tokenExpired } from '@/utils/dataUtils';
 
 const AuthContext = createContext<{
   signIn: (username: string, password: string) => Promise<void>;
@@ -29,70 +29,59 @@ export function useSession() {
 }
 
 export function SessionProvider(props: React.PropsWithChildren) {
-  console.log('SessionProvider');
   const [[isLoading, session], setSession] = useStorageState('session');
 
   useEffect(() => {
-    // Will refresh the access token with every axios request if the session exists
-    const requestInterceptor = axiosRequest.interceptors.request.use(
+    const requestInterceptor = axiosInstance.interceptors.request.use(
       async (config: any) => {
         if (session) {
-          config.headers.Authorization = `Bearer ${(session as { access?: string })?.access}`;
-          try {
-            console.log('trying request', config);
-            const { data } = await axios.post('token/refresh/', {
-              refresh: (session as { refresh?: string })?.refresh,
-            });
-            setSession({ ...session, access: data.access });
-            console.log('Session refreshed token:', data.access);
-          } catch (error) {
-            console.error('Failed to refresh token:', error);
-            setSession(null);
+          let accessToken = (session as { access?: string })?.access;
+
+          // Refresh the token if it is expired
+          if (accessToken && tokenExpired(accessToken)) {
+            try {
+              const { data } = await axios.post('token/refresh/', {
+                refresh: (session as { refresh?: string })?.refresh,
+              });
+
+              setSession({ ...session, access: data.access });
+              accessToken = data.access;
+              console.log('AxiosInterceptor - Refreshed session token:', data.access);
+            } catch (error) {
+              console.error('Failed to refresh token:', error);
+              setSession(null);
+            }
           }
+
+          // Finally set the access token in the Auth header
+          config.headers.Authorization = `Bearer ${accessToken}`;
         }
         return config;
       },
       (error) => Promise.reject(error),
     );
 
-    // NOTE: NEEDS WORK
-    // NOTE: This interceptor will only work for the first request after the session is set, needs more testing
-    const responseInterceptor = axiosRequest.interceptors.response.use(
+    // NOTE: Default response interceptor
+    const responseInterceptor = axiosInstance.interceptors.response.use(
       (response) => response,
       async (error) => {
-        // Set the original request config to a variable
-        const originalRequest = error.config;
-
-        if (error.response?.status === 401 && !originalRequest._retry && session) {
-          // Prevent infinite loop
-          originalRequest._retry = true;
-
-          try {
-            const { data } = await axios.post('token/refresh/', {
-              refresh: (session as { refresh?: string })?.refresh,
-            });
-            setSession({ ...session, access: data.access });
-            originalRequest.headers.Authorization = `Bearer ${data.access}`;
-            console.log('Axios request interceptor response, 401 error, token refreshed');
-
-            // Retry the original request
-            return axiosRequest(originalRequest);
-          } catch (error) {
-            console.error('Failed to refresh token:', error);
-            setSession(null);
-            return Promise.reject(error); // If future error handling is needed, best to reject the promise at this stage before checking for other error codes.
-          }
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
         } else {
-          console.error('Interceptor response error:', error);
-          console.error('Interceptor response error.config:', error.config);
+          console.log('Error', error.message);
         }
+        console.log(error.config);
         return Promise.reject(error);
       },
     );
 
     return () => {
-      axiosRequest.interceptors.request.eject(requestInterceptor);
-      axiosRequest.interceptors.response.eject(responseInterceptor);
+      axiosInstance.interceptors.request.eject(requestInterceptor);
+      axiosInstance.interceptors.response.eject(responseInterceptor);
     };
   }, [session]);
 
@@ -117,8 +106,7 @@ export function SessionProvider(props: React.PropsWithChildren) {
         },
         session: session as { access: string; refresh: string } | null,
         isLoading,
-      }}
-    >
+      }}>
       {props.children}
     </AuthContext.Provider>
   );
