4c777a5842446ef35b517a4286ccb486be9e1ea5
Destan <destantezel@googlemail.com>
2024-05-30 18:28:33 +0100

    Install and setup ESLint - import resolvers for TypeScript

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'import'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
   rules: {
     'prettier/prettier': 'warn',
+    'import/no-unresolved': 'error',
   },
 };
