d3b2208a65bc0d09e28b32f7222f4cbaa61a8556
Destan <destantezel@googlemail.com>
2024-06-11 22:31:29 +0100

    Simplify sign-in and up pages for the updated login and sign-up forms respectively

--- frontend/app/sign-up.tsx
+++ frontend/app/sign-up.tsx
@@ -3,7 +3,7 @@ import SignUpForm from '@/components/form/SignUpForm';
 
 export default function SignUp() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View>
       <SignUpForm />
     </View>
   );
