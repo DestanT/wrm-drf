a562f022fa24fff674f818d05350d7441c48c7b7
Destan <destantezel@googlemail.com>
2024-06-12 20:51:45 +0100

    Annotate the queryset to display a count of playlists for the user

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
 from wrm_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
@@ -5,12 +6,16 @@ from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Profile.objects.annotate(
+        playlist_count=Count('owner__playlists', distinct=True),
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
-    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        playlist_count=Count('owner__playlists', distinct=True),
+    )
