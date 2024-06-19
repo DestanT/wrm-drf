d10934f4e3be101168ad840595ff7f799fbf5f3e
Destan <destantezel@googlemail.com>
2024-06-19 11:39:05 +0100

    Extend cTailwind classes to include custom colour scheme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#1a1a1a',
+        neutralLight: '#ddd5ce',
+        neutralDark: '#b2ab9b',
+        buttonLight: '#7c7572',
+        buttonDark: '#3f3c3b',
+      },
+    },
   },
   plugins: [],
 };
